d93e08955ddc232b04666e8f973b52bddb3e03e6
garick-dev <73178499+garick-dev@users.noreply.github.com>
2021-10-04 01:06:12 +0300

    Add zoom for charts

--- main.js
+++ main.js
@@ -15,6 +15,8 @@ const calculateDataTableThreeEl = document.querySelector(".form-btn-calculate");
 
 let counter1 = 1;
 let counter2 = 1;
+let scaleX = 30;
+let scaleY = 30;
 
 ////////ADD ROWS//////////////
 
@@ -104,18 +106,32 @@ const getValueTable2 = (counter) => {
     return { arrXtable2, arrYtable2 };
   
 }
+const getValueTable3 = (arrXtable1, arrXtable2, arrYtable1, arrYtable2, counter) => {
+    let arrXtable3 = [];
+    let arrYtable3 = [];
+    
+    for (let i = 0; i < counter; i++) {
+
+        let resX = (Number(arrXtable1[i]) + Number(arrXtable2[i])) / 2;
+        let resY = (Number(arrYtable1[i]) + Number(arrYtable2[i])) / 2;
+        arrXtable3.push(resX);
+        arrYtable3.push(resY);
+    }
+    return { arrXtable3, arrYtable3};
+}
 ////////////////GET VALUE TABLE////////////////
 
 
 ////////////////RENDER TABLE 3////////////////
+
 
 const renderTable3 = (arrXtable1, arrXtable2, arrYtable1, arrYtable2, counter) => {
     let arrXtable3 = [];
     let arrYtable3 = [];
     for (let i = 0; i < counter; i++) {
 
-        resX = (Number(arrXtable1[i]) + Number(arrXtable2[i])) / 2;
-        resY = (Number(arrYtable1[i]) + Number(arrYtable2[i])) / 2;
+        let resX = (Number(arrXtable1[i]) + Number(arrXtable2[i])) / 2;
+        let resY = (Number(arrYtable1[i]) + Number(arrYtable2[i])) / 2;
         arrXtable3.push(resX);
         arrYtable3.push(resY);
             const resultForHtml = `<div class="form-row table-3" data-id="${i}">
@@ -129,14 +145,12 @@ const renderTable3 = (arrXtable1, arrXtable2, arrYtable1, arrYtable2, counter) =
 
 ////////////////RENDER TABLE 3///////////////
 
-
 ////////////////CALCULATE AND DRAW CHARTS/////////////////
 
 const calculateDataTable = () => {
     calculateDataTableThreeEl.addEventListener("click", (ev) => {
-        ev.preventDefault();
-      
-    
+        ev.preventDefault();           
+        
         let minCounter = 0;
         // Find min counter for 2 tables
         if (counter1 > counter2) {
@@ -148,12 +162,10 @@ const calculateDataTable = () => {
 
         // Сheck block on empty
          if (!dataFormThreeEl.hasChildNodes()) {
-            const { arrXtable1 } = getValueTable1(minCounter);
-            const { arrYtable1 } = getValueTable1(minCounter);
-            const { arrXtable2 } = getValueTable2(minCounter);
-            const { arrYtable2 } = getValueTable2(minCounter);
+            const { arrXtable1, arrYtable1  } = getValueTable1(minCounter);
+            const { arrXtable2,  arrYtable2 } = getValueTable2(minCounter);
             // Check value on empty
-            if (arrXtable1[0] === "" || arrYtable1[0] === "" || arrXtable2[0] === "" || arrXtable2[0] === "" || counter1 === 0 || counter2 === 0)  {
+            if (arrXtable1[0] === "" || arrYtable1[0] === "" || arrXtable2[0] === "" || arrYtable2[0] === "" || counter1 === 0 || counter2 === 0)  {
                 let resultHtml = `<h3 class="message"> Введите пожалуйста данные </h3>`; 
                 // Add a message if there is none  (there once)
                  if (!contentBlockEl.classList.contains("message")) {              
@@ -171,10 +183,27 @@ const calculateDataTable = () => {
             })    
             //  Get value table 3 and draw charts       
             const { arrXtable3 , arrYtable3  } = renderTable3(arrXtable1, arrXtable2, arrYtable1, arrYtable2, minCounter);
-            drawTable1(arrXtable1, arrYtable1);
-            drawTable2(arrXtable2, arrYtable2);
-            drawTable3(arrXtable3, arrYtable3);
+
+            const objTable = { 
+                arrXtable : {
+                  1: arrXtable1,
+                  2:  arrXtable2, 
+                  3: arrXtable3               
+                },
+                arrYtable: {
+                    1: arrYtable1,
+                    2:  arrYtable2, 
+                    3: arrYtable3               
+                  },    
+             }
+            for (let i = 1; i <= 3; i++) {
+                
+               drawTable(objTable.arrXtable[i], objTable.arrYtable[i], scaleX, scaleY, i);
+               zoomTable(objTable.arrXtable[i], objTable.arrYtable[i], scaleX, scaleY, i);
             }
+            
+          
+           }
         }   
         else { 
             return;
@@ -187,23 +216,25 @@ const calculateDataTable = () => {
 
 /////////////////////////CANVAS////////////////////
 
-const drawTable1 = (arrX, arrY) => {
 
-    const canvasPlot = document.querySelector(".canvas-plot-1");
-    const ctx = canvasPlot.getContext("2d");
+///////////////////////DRAW TABLE //////////////////////////
+const drawTable = (arrX, arrY, scaleX, scaleY, id) => {
 
     
-        const canvasPlotWidth = canvasPlot.clientWidth;
-        const canvasPlotHeight = canvasPlot.clientHeight;
+    const canvasPlot = document.querySelector(`.canvas-plot-${id}`);
+    const ctx = canvasPlot.getContext("2d");
+     ctx.clearRect(0, 0, canvasPlot.width, canvasPlot.height);
 
-        const scaleX = 30;
-        const scaleY = 30;
+     const canvasPlotWidth = canvasPlot.width;
+     const canvasPlotHeight = canvasPlot.height;    
+     
+
+     const xAxis = Math.round( canvasPlotWidth / scaleX  / 2) * scaleX ;
+     const yAxis = Math.round( canvasPlotHeight / scaleY  / 2) * scaleY;
+
         const shiftNumberNames = 5;
         const shiftAxisNames = 20;
-
-        
-        const xAxis = Math.round( canvasPlotWidth / scaleX  / 2) * scaleX ;
-        const yAxis = Math.round( canvasPlotHeight / scaleY  / 2) * scaleY;
+       
         ctx.font = `${Math.round(scaleX / 2)}px Arial`;
         ctx.textAlign = "left";
         ctx.textBaseline = "top";
@@ -212,7 +243,6 @@ const drawTable1 = (arrX, arrY) => {
 // Background grid
     ctx.beginPath();
     ctx.strokeStyle = "#f5f0e8";
-    
     for (let i = 0; i <= canvasPlotWidth; i = i + scaleX) {
         ctx.moveTo(i, 0);
         ctx.lineTo(i, canvasPlotHeight);
@@ -265,165 +295,42 @@ const drawTable1 = (arrX, arrY) => {
 
     }
 }
-const drawTable2 = (arrX, arrY) => {
+///////////////////////DRAW TABLE //////////////////////////
 
-    const canvasPlot = document.querySelector(".canvas-plot-2");
-    const ctx = canvasPlot.getContext("2d");
 
+
+///////////////////////ZOOM TABLE//////////////////////////////
+const zoomTable = (arrX, arrY, scaleX, scaleY, id) => {
+          
+    const canvasPlot = document.querySelector(`.canvas-plot-${id}`);
+
+    canvasPlot.addEventListener("wheel", (ev) => {
+        ev.preventDefault();    
+
+        canvasPlot.scrollIntoView( { block: "center"} );    
+         
+            
+          ev = ev || window.event;
     
-        const canvasPlotWidth = canvasPlot.clientWidth;
-        const canvasPlotHeight = canvasPlot.clientHeight;
+        let delta = ev.deltaY || ev.detail || ev.wheelDelta;
+        console.log(scaleX, scaleY);
+        if (delta < 0 && scaleX < 100 && scaleY < 100) {
+             scaleX += 1;
+             scaleY += 1;
+        }
+      else if (delta > 0 && scaleX > 10 && scaleY > 10) {
+          scaleX -= 1;
+          scaleY -= 1;
+      }
 
-        const scaleX = 30;
-        const scaleY = 30;
-        const shiftNumberNames = 5;
-        const shiftAxisNames = 20;
-
-        const xAxis = Math.round( canvasPlotWidth / scaleX  / 2) * scaleX ;
-        const yAxis = Math.round( canvasPlotHeight / scaleY  / 2) * scaleY;
-
-        ctx.font = `${Math.round(scaleX / 2)}px Arial`;
-        ctx.textAlign = "left";
-        ctx.textBaseline = "top";
+        drawTable(arrX, arrY, scaleX, scaleY, id);
     
-        
-// Background grid
-    ctx.beginPath();
-    ctx.strokeStyle = "#f5f0e8";
-    
-    for (let i = 0; i <= canvasPlotWidth; i = i + scaleX) {
-        ctx.moveTo(i, 0);
-        ctx.lineTo(i, canvasPlotHeight);
-        ctx.fillText((i - xAxis) / scaleX, i + shiftNumberNames, yAxis + shiftNumberNames);
-        ctx.fillRect(i - shiftNumberNames / 4, yAxis - shiftNumberNames, 2, 10);
-    }
-    
-    for (let i = 0; i <= canvasPlotHeight; i = i + scaleY) {
-        ctx.moveTo(0 , i);
-        ctx.lineTo( canvasPlotWidth , i);
-        ctx.fillText((yAxis - i ) / scaleY, xAxis + shiftNumberNames , i + shiftNumberNames);
-        ctx.fillRect(xAxis - shiftNumberNames , i - shiftNumberNames / 4, 10, 2);
-    }
-    ctx.stroke();
-    ctx.closePath();
-
-
-// Main line 
-
-    ctx.beginPath();
-    ctx.strokeStyle = "#000000";
-    ctx.moveTo(xAxis, 0);
-    ctx.lineTo(xAxis, canvasPlotHeight);
-    ctx.fillText("y", xAxis - shiftAxisNames, 0);
-
-    ctx.moveTo(0, yAxis);
-    ctx.lineTo(canvasPlotWidth, yAxis);
-    ctx.fillText("x", canvasPlotWidth - shiftAxisNames, yAxis-shiftAxisNames);
-
-    ctx.stroke();
-    ctx.closePath();
-
-
-
-// Draw line 
-
-    let x, y, xNext, yNext  = "";
-
-    ctx.fillStyle = "#000000"
-    for (let j = 0; j < arrX.length; j++) {
-
-    x = Number(arrX[j]);
-    y = Number(arrY[j]);
-    xNext = Number(arrX[j + 1]);
-    yNext = Number(arrY[j + 1]);
-    ctx.fillRect((x * scaleX) + xAxis, yAxis - (scaleY * y), 1, 1); 
-    ctx.moveTo(x * scaleX + xAxis, yAxis - scaleY * y);   
-    ctx.lineTo( xNext * scaleX + xAxis, yAxis - scaleY * yNext);
-    ctx.stroke();
-
-    }
+    });
 }
-const drawTable3 = (arrX, arrY) => {
+///////////////////////ZOOM TABLE//////////////////////////////
 
-    const canvasPlot = document.querySelector(".canvas-plot-3");
-    const ctx = canvasPlot.getContext("2d");
-
-    
-        const canvasPlotWidth = canvasPlot.clientWidth;
-        const canvasPlotHeight = canvasPlot.clientHeight;
-
-        const scaleX = 30;
-        const scaleY = 30;
-        const shiftNumberNames = 5;
-        const shiftAxisNames = 20;
-
-        const xAxis = Math.round( canvasPlotWidth / scaleX  / 2) * scaleX ;
-        const yAxis = Math.round( canvasPlotHeight / scaleY  / 2) * scaleY;
-
-        ctx.font = `${Math.round(scaleX / 2)}px Arial`;
-        ctx.textAlign = "left";
-        ctx.textBaseline = "top";
-    
-        
-// Background grid
-    ctx.beginPath();
-    ctx.strokeStyle = "#f5f0e8";
-    
-    for (let i = 0; i <= canvasPlotWidth; i = i + scaleX) {
-        ctx.moveTo(i, 0);
-        ctx.lineTo(i, canvasPlotHeight);
-        ctx.fillText((i - xAxis) / scaleX, i + shiftNumberNames, yAxis + shiftNumberNames);
-        ctx.fillRect(i - shiftNumberNames / 4, yAxis - shiftNumberNames, 2, 10);
-    }
-    
-    for (let i = 0; i <= canvasPlotHeight; i = i + scaleY) {
-        ctx.moveTo(0 , i);
-        ctx.lineTo( canvasPlotWidth , i);
-        ctx.fillText((yAxis - i ) / scaleY, xAxis + shiftNumberNames , i + shiftNumberNames);
-        ctx.fillRect(xAxis - shiftNumberNames , i - shiftNumberNames / 4, 10, 2);
-    }
-    ctx.stroke();
-    ctx.closePath();
-
-
-// Main line 
-
-    ctx.beginPath();
-    ctx.strokeStyle = "#000000";
-    ctx.moveTo(xAxis, 0);
-    ctx.lineTo(xAxis, canvasPlotHeight);
-    ctx.fillText("y", xAxis - shiftAxisNames, 0);
-
-    ctx.moveTo(0, yAxis);
-    ctx.lineTo(canvasPlotWidth, yAxis);
-    ctx.fillText("x", canvasPlotWidth - shiftAxisNames, yAxis-shiftAxisNames);
-
-    ctx.stroke();
-    ctx.closePath();
-
-
-
-// Draw line 
-
-    let x, y, xNext, yNext  = "";
-
-    ctx.fillStyle = "#000000"
-    for (let j = 0; j < arrX.length; j++) {
-
-    x = Number(arrX[j]);
-    y = Number(arrY[j]);
-    xNext = Number(arrX[j + 1]);
-    yNext = Number(arrY[j + 1]);
-    ctx.fillRect((x * scaleX) + xAxis, yAxis - (scaleY * y), 1, 1); 
-    ctx.moveTo(x * scaleX + xAxis, yAxis - scaleY * y);   
-    ctx.lineTo( xNext * scaleX + xAxis, yAxis - scaleY * yNext);
-    ctx.stroke();
-
-    }
-}
 
 /////////////////////////CANVAS////////////////////
-
 
 
 addRowsForTableOne();
